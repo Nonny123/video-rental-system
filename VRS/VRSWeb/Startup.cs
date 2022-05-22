d218fb2ce007344a5722256f9c498bce333ebc4c
Nonso <justcoolk@yahoo.com>
2022-05-22 13:32:20 +0100

    Setup project

---
+++ VRS/VRSWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VRSWeb.Startup))]
+namespace VRSWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
