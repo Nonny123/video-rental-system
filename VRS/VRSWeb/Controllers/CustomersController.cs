5c8f8d4efc9444250ea6f728cfaddcbe6432de4b
Nonso <justcoolk@yahoo.com>
2022-05-25 18:02:16 +0100

    Add/Edit customer record

--- VRS/VRSWeb/Controllers/CustomersController.cs
+++ VRS/VRSWeb/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VRSWeb.Models;
+using VRSWeb.ViewModels;
 
 namespace VRSWeb.Controllers
 {
@@ -20,6 +21,39 @@ namespace VRSWeb.Controllers
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm",viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                
+                //Mapper.Map(customer, customerInDb);
+                
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
         }
 
         // GET: Customers
@@ -43,6 +77,21 @@ namespace VRSWeb.Controllers
             return View(customer);
         }
 
-       
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+
+            };
+            return View("CustomerForm", viewModel);
+        }
     }
 }
--- VRS/VRSWeb/Models/Customer.cs
+++ VRS/VRSWeb/Models/Customer.cs
@@ -14,11 +14,14 @@ namespace VRSWeb.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
         public MembershipType MembershipType { get; set; } //nav property - for loading an object and its related object
+        
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; } //FK for when the FKID only is needed and not the related object
     }
 }
