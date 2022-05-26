4e0f26ec23a9f49df3b9d2f2a7419e80f4cc03bf
Nonso <justcoolk@yahoo.com>
2022-05-26 08:42:58 +0100

    Add/edit movie record

--- VRS/VRSWeb/Controllers/MoviesController.cs
+++ VRS/VRSWeb/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VRSWeb.Models;
+using VRSWeb.ViewModels;
 
 namespace VRSWeb.Controllers
 {
@@ -29,6 +30,35 @@ namespace VRSWeb.Controllers
             return View(movies);
         }
 
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+
         public ActionResult Details(int id)
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
@@ -38,6 +68,29 @@ namespace VRSWeb.Controllers
 
             return View(movie);
 
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
         // GET: Movies
         public ActionResult Random()
@@ -51,5 +104,7 @@ namespace VRSWeb.Controllers
         {
             return Content(year + "/" + month);
         }
+
+        
     }
 }
