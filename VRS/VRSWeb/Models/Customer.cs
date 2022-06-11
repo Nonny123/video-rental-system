d4bf53ea3d0f1d12d299e4358a9b9a4dac093894
Nonso <justcoolk@yahoo.com>
2022-06-11 15:52:11 +0100

    Custom validation

--- VRS/VRSWeb/Models/Customer.cs
+++ VRS/VRSWeb/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace VRSWeb.Models
     {
         public int Id { get; set; } 
         
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -22,6 +22,7 @@ namespace VRSWeb.Models
         public MembershipType MembershipType { get; set; } //nav property - for loading an object and its related object
         
         [Display(Name = "Membership Type")]
+        [Min18YearsIfAMember]
         public byte MembershipTypeId { get; set; } //FK for when the FKID only is needed and not the related object
     }
 }
---
+++ VRS/VRSWeb/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VRSWeb.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+                                     //gives access to the containing class (e.g customer)
+            var customer = (Customer) validationContext.ObjectInstance;
+
+            if(customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required.");
+
+        }
+    }
+}
