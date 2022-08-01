7a14ef35de3a78d5135ea59fe773aceb64fdf61b
Nonso <justcoolk@yahoo.com>
2022-08-01 01:44:49 +0100

    Cache rendered HTML

--- VRS/VRSWeb/Controllers/HomeController.cs
+++ VRS/VRSWeb/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace VRSWeb.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam = "genre")]
+        //[OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)] - disable caching
         public ActionResult Index()
         {
             return View();
